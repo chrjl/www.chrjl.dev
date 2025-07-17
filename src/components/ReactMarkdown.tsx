e6bbcc398c1a33e8fefb79840fd678770617d467
chrjl <73675826+chrjl@users.noreply.github.com>
2025-07-17 15:29:54 -0700

    build: implement Markdown <dl> via plugin

--- src/components/ReactMarkdown.tsx
+++ src/components/ReactMarkdown.tsx
@@ -1,10 +1,16 @@
 import ReactMarkdown from 'react-markdown';
 import rehypeRaw from 'rehype-raw';
 import rehypeExternalLinks from 'rehype-external-links';
+import {
+  remarkDefinitionList,
+  defListHastHandlers,
+} from 'remark-definition-list';
 
 const Markdown = ({ children }: { children: string }) => (
   <ReactMarkdown
+    remarkPlugins={[remarkDefinitionList]}
     rehypePlugins={[rehypeRaw, [rehypeExternalLinks, { target: '_blank' }]]}
+    remarkRehypeOptions={{ handlers: defListHastHandlers }}
   >
     {children}
   </ReactMarkdown>
