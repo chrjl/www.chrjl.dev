4b1b809a378d1855c3d1af1d9219593f7d21c14b
chrjl <73675826+chrjl@users.noreply.github.com>
2025-07-23 06:03:53 -0700

    feat: scroll state fallback using IntersectionObserver

--- src/layouts/ScrollSnapContainer.tsx
+++ src/layouts/ScrollSnapContainer.tsx
@@ -1,6 +1,33 @@
+import { useEffect } from 'react';
+
 import '../styles/components/scroll-snap.scss';
 
 const ScrollSnapContainer = ({ children }: { children: React.ReactNode }) => {
+  useEffect(() => {
+    // Fallback if browser does not support scroll snap container query.
+    // Use Intersection Observer and CSS class to emulate behavior.
+
+    if (CSS.supports('container-type', 'scroll-state')) {
+      return;
+    }
+
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('card-snapped-fallback');
+        } else {
+          entry.target.classList.remove('card-snapped-fallback');
+        }
+      },
+      {
+        threshold: [1],
+      }
+    );
+
+    const cards = document.querySelectorAll('.card');
+    cards.forEach((card) => observer.observe(card));
+  }, []);
+
   return children;
 };
 
