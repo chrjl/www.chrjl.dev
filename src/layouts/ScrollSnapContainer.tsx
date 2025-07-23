20e67d9f42204bdb7fd09e2a6def1c330bcf47ac
chrjl <73675826+chrjl@users.noreply.github.com>
2025-07-23 06:01:58 -0700

    feat (Card): scroll snap, scroll state styles

---
+++ src/layouts/ScrollSnapContainer.tsx
@@ -0,0 +1,7 @@
+import '../styles/components/scroll-snap.scss';
+
+const ScrollSnapContainer = ({ children }: { children: React.ReactNode }) => {
+  return children;
+};
+
+export default ScrollSnapContainer;
